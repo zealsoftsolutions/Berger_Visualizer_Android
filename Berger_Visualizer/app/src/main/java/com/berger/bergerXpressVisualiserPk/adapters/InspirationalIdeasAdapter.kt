a94d988a8a9d1c073e7c420c5e0e6b71c0097f7d
Ahtesham-Azhar <ahteshamazhar@gmail.com>
2026-06-24 20:58:35 +0500

    OpenCV implementation changed from local library integration to gradle import, Home page products card background changed, minor changes

--- Berger_Visualizer/app/src/main/java/com/berger/bergerXpressVisualiserPk/adapters/InspirationalIdeasAdapter.kt
+++ Berger_Visualizer/app/src/main/java/com/berger/bergerXpressVisualiserPk/adapters/InspirationalIdeasAdapter.kt
@@ -2,10 +2,8 @@ package com.berger.bergerXpressVisualiserPk.adapters
 
 import android.app.Activity
 import android.content.Intent
-import android.graphics.Bitmap
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.Toast
 import com.berger.bergerXpressVisualiserPk.R
 import com.berger.bergerXpressVisualiserPk.activities.ViewImageActivity
 import com.berger.bergerXpressVisualiserPk.models.Idea
@@ -13,15 +11,6 @@ import com.berger.bergerXpressVisualiserPk.utill.Constants
 import com.berger.bergerXpressVisualiserPk.utill.Utills
 import com.berger.bergerXpressVisualiserPk.viewHolders.AlbumListViewHolder
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.DecodeFormat
-import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.RequestOptions
-import com.bumptech.glide.request.target.Target
-import com.bumptech.glide.signature.MediaStoreSignature
-import java.util.*
 
 class InspirationalIdeasAdapter  (val activity: Activity, private val images: List<Idea>) : androidx.recyclerview.widget.RecyclerView.Adapter<AlbumListViewHolder>() {
 
